8d0e990df4a9cf61f05d076a4111575fde293658
bmh1211 <bmh1211@gmail.com>
2021-02-16 16:57:31 +0900

    11279_최대힙 1927_최소힙 11286_절댓값힙

---
+++ baekjoon/11279.cpp
@@ -0,0 +1,33 @@
+// ÃÖ´ë Èü
+
+#include<iostream>
+#include<queue>
+using namespace std;
+
+priority_queue<int, vector<int>, less<int> > pq;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		int temp, index_count = 0;
+		cin >> temp;
+
+		if (temp > 0) {
+			pq.push(temp);
+		}
+		else if (temp == 0) {
+			if (pq.empty())
+				cout << 0 << "\n";
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ baekjoon/11286.cpp
@@ -0,0 +1,57 @@
+// 절댓값 힙
+
+#include<iostream>
+#include<queue>
+using namespace std;
+
+struct number {
+	int input;
+	int origin;
+};
+
+struct compare {
+	bool operator()(number a, number b) {
+		if (a.input != b.input)
+			// 부모의 절대값(input)이 더 클 경우 스왑
+			return a.input > b.input;
+		else
+			// 부모의 원래값(origin)이 더 클 경우 스왑
+			return a.origin > b.origin;
+	}
+};
+
+priority_queue<number, vector<number>, compare> pq;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		int temp, index_count = 0;
+		cin >> temp;
+
+		if (temp == 0) {
+			if (pq.empty())
+				cout << 0 << "\n";
+			else {
+				cout << pq.top().origin << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			number num;
+
+			if (temp < 0)
+				num.input = temp * -1;
+			else
+				num.input = temp;
+			num.origin = temp;
+
+			pq.push(num);
+		}
+	}
+
+	return 0;
+}
